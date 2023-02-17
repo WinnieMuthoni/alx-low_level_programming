8b1aff8862c50fed0d0a7a77407a7688a0f14fa5
WinnieMuthoni <winniemsoo58@gmail.com>
2023-02-17 04:09:35 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,8 +5,8 @@
 /**
  * main - Entry point
  *
- * Description: This program will assign a random number to the variable n each time it is executed.
- *              It prints whether the number stored in the variable n is positive, negative, or zero.
+ * This program will assign a random number to the variable n each time it is executed.
+ * Complete the source code in order to print whether the number stored in the variable n is positive or negative.
  *
  * Return: Always 0 (Success)
  */
