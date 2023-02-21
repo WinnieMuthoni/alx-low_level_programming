a07110b384c8f03e38fb70ad39434a14287e2c15
WinnieMuthoni <winniemsoo58@gmail.com>
2023-02-21 03:09:43 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,11 @@
-#include <main.h>
-
-int main()
+#include <stdio.h>
+#include "main.h"
+/**
+ *main-Check description
+ *Description:It prints the word _putchar, followed by a ne line.
+ *Return: 0.
+ */
+int main(void)
 {
   printf("_putchar\n");
   return 0;
