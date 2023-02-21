e7ba9ce640cc6095a030ad7d5b8b47406a7e090c
WinnieMuthoni <winniemsoo58@gmail.com>
2023-02-21 15:13:17 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,21 +12,13 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	{
-		putchar(43);
-		return (1);
-	}
-	else if (n < 0)
-	{
-		putchar(45);
-		return (-1);
-	}
-	else
-	{
-		putchar(48);
-		return (0);
-	}
-	putchar('\n');
+	printf("+");
+        return 1;
+    } else if (n == 0) {
+        printf("0");
+        return 0;
+    } else {
+        printf("-");
+        return -1;
+    }
 }
-
-
