74fa91cd48d6e3eb6a5bb6bccd7f4439cfd43509
WinnieMuthoni <winniemsoo58@gmail.com>
2023-02-21 14:24:03 -0800

    5-main.c

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -6,7 +6,6 @@
  *
  * Return: Always 0.
  */
-int print_sign(int n);
 
 int main(void)
 {
@@ -33,4 +32,40 @@ int main(void)
     _putchar(r + '0');
     _putchar('\n');
     return (0);
+}
+int print_sign(int n)
+{
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else
+    {
+        _putchar('-');
+        return (-1);
+    }
+}
+int print_sign(int n)
+{
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else
+    {
+        _putchar('-');
+        return (-1);
+    }
 }
