8704f334e843c58308668a3552ccec52ce8584eb
WinnieMuthoni <winniemsoo58@gmail.com>
2023-02-21 15:22:31 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,10 +1,7 @@
-#include <stdio.h>
-#include <ctype.h>
 #include "main.h"
 
 /**
- * main - check the code.
- *This file was intentionally left empty
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
@@ -12,14 +9,14 @@ int main(void)
 {
     int r;
 
-    r = isalpha('H');
-    putchar(r + '0');
-    r = isalpha('o');
-    putchar(r + '0');
-    r = isalpha(108);
-    putchar(r + '0');
-    r = isalpha(';');
-    putchar(r + '0');
-    putchar('\n');
+    r = _isalpha('H');
+    _putchar(r + '0');
+    r = _isalpha('o');
+    _putchar(r + '0');
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
+    _putchar(r + '0');
+    _putchar('\n');
     return (0);
 }
