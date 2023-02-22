9068dc2e4770337eeb9e2d01420a66a1667bd124
WinnieMuthoni <winniemsoo58@gmail.com>
2023-02-21 16:15:23 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
@@ -14,5 +14,12 @@ int main(void)
     print_times_table(98);
     _putchar('\n');
     print_times_table(12);  
-    return (0);
+   _putchar('\n');
+    print_times_table(15);
+	_putchar('\n');
+    print_times_table(16);
+	_putchar('\n');
+    print_times_table(0);
+	_putchar('\n');
+	 return (0);
 }
