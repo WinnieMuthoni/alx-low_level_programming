67bd53af8b5a961810d9540b39961aae11be9c87
WinnieMuthoni <winniemsoo58@gmail.com>
2023-02-21 17:11:13 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -5,15 +5,15 @@
  *
  * Return: Always 0.
  */
-void print_times_table(int n) {
+void print_times_table(void)
 int main(void)
 {
     times_table(3);
     _putchar('\n');
-    times_table(5);
+    print_times_table(5);
     _putchar('\n');
-    times_table(98);
+    print_times_table(98);
     _putchar('\n');
-    times_table(12);  
+    print_times_table(12);  
     return (0);
 }
