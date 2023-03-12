bcc72edd9cf59d44c683ff1b2cfa9aa1fad7d55c
WinnieMuthoni <winniemsoo58@gmail.com>
2023-03-12 08:08:05 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,17 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
-* main - prints its name follwoed by a newline
-* @argc: number of arguments
-* @argv: the vector that hold the arguments
-*
-* Description: prints the current directory and the name like ./name
-* Return: returns the name.
-*/
+ * main - is a function that prints the name.
+ *@argc: is an argument counter for char.
+ *@argv: is an agument value for char.
+ *Return: Returns always success.
+**/
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc - 1]);
-	return (0);
+int i = argc - argc;
+
+printf("%s\n", argv[i]);
+
+return (0);
 }
