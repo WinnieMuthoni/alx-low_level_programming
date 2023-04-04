5a00f5c5cfed64c0346b713d12fbf19b01f131ea
Winnie Muthoni <winniemsoo58@gmail.com>
2023-04-04 04:18:17 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,14 +2,16 @@
 
 size_t print_listint(const listint_t *h)
 {
+
   size_t nodes;
+
   nodes = 0;
   while(h)
     {
       printf("%d\n", h->n);
       nodes++;
       h = h->next;
-    }
-  return (nodes);
 
-}  
+    }
+  return (nodes)
+}
