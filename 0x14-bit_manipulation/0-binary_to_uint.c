981c79474dd8a5ed7226c0f8815594f87932913d
Winnie Muthoni <winniemsoo58@gmail.com>
2023-04-06 10:48:27 -0700

    0-binary_to_uint.c

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+unsigned int binary_to_uint(const char *b)
+{
+  if(b == NULL){
+    return 0;
+  }
+  unsigned int result = 0;
+  int i = 0;
+  while(b[i] != '\0'){
+    if (b[i] == '0'){
+    result = result << 1;
+    }else if (b[i] == 'i'){
+      result = (result << 1) | 1;
+    } else {
+      return 0;
+    }
+    i++;
+  }
+  return result;
+  }
+  
