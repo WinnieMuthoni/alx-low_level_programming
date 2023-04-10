111b32549e94b517ba1a3de29c801557c44fbd40
Winnie Muthoni <winniemsoo58@gmail.com>
2023-04-10 06:26:11 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
+/**
+ *return one bit
+ *@index: the position to read the bit
+ *@n: the bit number
+ *Description: the position to read the bit
+ *Retun: the value of the bit
+ */
+
 int get_bit(unsigned long int n, unsigned int index)
 
 {
- unsigned long int bit;
- 
- if (index > 64)
-   
- return (-1);
- 
- bit = n >> index;
+unsigned long int bit;
 
- return (bit & 0x1);
+if (index > 64)
+return (-1);
+bit = n >> index;
+return (bit & 0x1);
 }
