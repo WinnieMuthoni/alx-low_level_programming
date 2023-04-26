76ab3c703b4dc83219f50aa12ff66ab13e1ffdc6
Winnie Muthoni <winniemsoo58@gmail.com>
2023-04-26 02:58:02 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,32 +1,30 @@
 #include "main.h"
-
-
-
+/**
+ * print_binary - decimal to binary without use / %
+ * @n: the decimal
+ * Description: convert decimal to binary
+ * section header: the header of this function is main.h
+ * Return: no return
+ */
 void print_binary(unsigned long int n)
-
 {
-  int num = 0;
-  unsigned long int numeral = 1;
+	int flag = 0;
+	unsigned long int mask = 1;
 
+	mask <<= 63;
+	if (n == 0)
+		_putchar('0');
 
-  num <<= 63;
-  if(n == 0)
-    _putchar('0');
+	while (mask > 0)
+	{
+		if ((n & mask) == 0 && flag == 1)
+			_putchar('0');
+		if ((n & mask) != 0)
+		{
+			_putchar('1');
+			flag = 1;
+		}
 
-
-  while(numeral > 0)
-    {
-      if((n & numeral) == 0 && num == 1)
-        _putchar('0');
-
-      if((n & num) != 0)
-      {
-        _putchar('1');
-        num = 1;
-      }
-
-      numeral = numeral >> 1;
-
-    }
+		mask = mask >> 1;
+	}
 }
-
