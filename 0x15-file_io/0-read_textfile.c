f336dd230fb10cb5bef224234ee013a6b916a312
Winnie Muthoni <winniemsoo58@gmail.com>
2023-04-25 23:19:29 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,42 +1,45 @@
 #include "main.h"
+
 /**
  * read_textfile - read a file.
  * @filename: the file to read.
- * @letters: the number of chars to read.
- * Description: read a file.
- * section header of this function is main.h
- * Return: return the number of chars.
-*/
-
+ * @letters: the number of chars to read
+ * Description: read a file
+ * section header: the header of this function is holberton.h
+ * Return: this return the number of chars.
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd, _read, _write;
-  char *buffer;
+	int f, _read, _write;
+	char *buff;
 
-  if (filename == NULL)
-    return (0);
+	if (filename == NULL)
+		return (0);
 
-  buffer = malloc(sizeof(char) * letters);
-  if (buffer == NULL)
-    return (0);
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
 
-  fd = open(filename, 0_RDONLY, 600);
-  if (fd == -1)
-    {
-      free(buffer);
-      return (0);
-    }
-  _read = read(fd, buffer, letters);
-  if (_read == -1)
-    {
-      free(buffer);
-      return (0);
-    }
-  _write = write(STDOUT_FILENO, buffer, _read);
-  if (_write == -1 || _write != _read)
-    {
-      return (0);
-    }
-  free(buffer);
-  close(fd);
-  return (_write);
+	f = open(filename, O_RDONLY, 600);
+	if (f == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	_read = read(f, buff, letters);
+	if (_read == -1)
+	{
+		free(buff);
+		return (0);
+	}
+
+	_write = write(STDOUT_FILENO, buff, _read);
+	if (_write == -1 || _write != _read)
+	{
+		return (0);
+	}
+
+	free(buff);
+	close(f);
+	return (_write);
+}
