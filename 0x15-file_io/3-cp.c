9d4b023669f7f993dde1edaf2cfeac79d86496cb
Winnie Muthoni <winniemsoo58@gmail.com>
2023-04-26 00:14:06 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,25 +8,25 @@
  */
 int main(int argc, char *argv[])
 {
-	int srce_fd, destn_fd;
-	int _read, _close_srce, _close_destn;
-	char buff[1024];
+	int src_fd, dest_fd;
+	int _read, _close_src, _close_dest;
+	char buffer[1024];
 
 	if (argc > 3 || argc < 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: copy file_from file_to\n");
 		exit(97);
 	}
-	srce_fd = open(*(argv + 1), O_RDONLY);
-	if (srce_fd == -1)
+	src_fd = open(*(argv + 1), O_RDONLY);
+	if (src_fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Cannot read from file %s\n", *(argv + 1));
 		exit(98);
 	}
-	destn_fd = open(*(argv + 2), O_TRUNC | O_CREAT | O_WRONLY, 0664);
-	while ((_read = read(srce_fd, buffer, 1024)) > 0)
+	dest_fd = open(*(argv + 2), O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	while ((_read = read(src_fd, buffer, 1024)) > 0)
 	{
-		if (destn_fd == -1 || (write(destn_fd, buffer, _read) != _read))
+		if (dest_fd == -1 || (write(dest_fd, buffer, _read) != _read))
 		{
 			dprintf(STDERR_FILENO, "Error: Cannot write to %s\n", *(argv + 2));
 			exit(99);
@@ -37,11 +37,11 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Cannot read from file %s\n", *(argv + 1));
 		exit(98);
 	}
-	_close_srce = close(srce_fd);
-	if (_close_srce < 0)
-		dprintf(STDERR_FILENO, "Error: Cannot close fd %i\n", srce_fd), exit(100);
-	_close_destn = close(destn_fd);
-	if (_close_destn < 0)
-		dprintf(STDERR_FILENO, "Error: Cannot close fd %i\n", destn_fd), exit(100);
+	_close_src = close(src_fd);
+	if (_close_src < 0)
+		dprintf(STDERR_FILENO, "Error: Cannot close fd %i\n", src_fd), exit(100);
+	_close_dest = close(dest_fd);
+	if (_close_dest < 0)
+		dprintf(STDERR_FILENO, "Error: Cannot close fd %i\n", dest_fd), exit(100);
 	return (0);
 }
